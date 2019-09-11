04c704ab3912ad5b7d7969cb1570b9769e3ffa73
Ting Luo <tin831@gmail.com>
2019-09-11 16:05:44 -0700

    added more javascript content, more CSS, and more index.html, updated README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lab-2
 About Me
+
+New features:
+- added more guessing games
+- added a score counter
+- added more styling
--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var name = prompt ('What is your name?');
+var name = prompt('What is your name?');
 
 alert('Hello ' + name + '! ' + 'This is Ting. Please guess y/n or yes/no to the following 5 questions about me.');
 
 
-var cat = prompt ('Do I have cats?');
+var cat = prompt('Do I have cats?');
+var counter = 0;
 
-if(cat.toLowerCase() === 'yes' || cat.toLowerCase() === 'y')
-{
+if (cat.toLowerCase() === 'yes' || cat.toLowerCase() === 'y') {
   console.log('Correct!');
+  counter = counter + 1;
   alert('Correct!');
-} else if(cat.toLowerCase() === 'no' || cat.toLowerCase() === 'n')
-{
+} else if (cat.toLowerCase() === 'no' || cat.toLowerCase() === 'n') {
   console.log('Incorrect!');
   alert('Incorrect!');
 } else {
@@ -21,59 +21,27 @@ if(cat.toLowerCase() === 'yes' || cat.toLowerCase() === 'y')
 }
 
 
-var dog = prompt ('Do I have dogs?');
+var dog = prompt('Do I have dogs?');
 
-if(dog.toLowerCase() === 'yes' || dog.toLowerCase() === 'y')
-{
+if (dog.toLowerCase() === 'yes' || dog.toLowerCase() === 'y') {
   console.log('Incorrect!');
   alert('Incorrect!');
-} else if(dog.toLowerCase() === 'no' || dog.toLowerCase() === 'n')
-{
+} else if (dog.toLowerCase() === 'no' || dog.toLowerCase() === 'n') {
   console.log('Correct!');
+  counter = counter + 1;
   alert('Correct!');
 } else {
   console.log('Please input y/n or yes/no');
   alert('Please input y/n or yes/no');
 }
 
-var icecream = prompt ('Do I like ice cream?');
+var icecream = prompt('Do I like ice cream?');
 
-if(icecream.toLowerCase() === 'yes' || icecream.toLowerCase() === 'y')
-{
+if (icecream.toLowerCase() === 'yes' || icecream.toLowerCase() === 'y') {
   console.log('Correct!');
+  counter = counter + 1;
   alert('Correct!');
-} else if(icecream.toLowerCase() === 'no' || icecream.toLowerCase() === 'n')
-{
-  console.log('Incorrect!');
-  alert('Incorrect!');
-} else {
-  console.log('Please input y/n or yes/no');
-  alert('Please input y/n or yes/no');
-}
-
-var surf = prompt ('Do I surf?');
-
-if(surf.toLowerCase() === 'yes' || surf.toLowerCase() === 'y')
-{
-  console.log('Incorrect!');
-  alert('Incorrect!');
-} else if(surf.toLowerCase() === 'no' || surf.toLowerCase() === 'n')
-{
-  console.log('Correct!');
-  alert('Correct!');
-} else {
-  console.log('Please input y/n or yes/no');
-  alert('Please input y/n or yes/no');
-}
-
-var tea = prompt ('Do I drink tea?');
-
-if(tea.toLowerCase() === 'yes' || tea.toLowerCase() === 'y')
-{
-  console.log('Correct!');
-  alert('Correct!');
-} else if(tea.toLowerCase() === 'no' || tea.toLowerCase() === 'n')
-{
+} else if (icecream.toLowerCase() === 'no' || icecream.toLowerCase() === 'n') {
   console.log('Incorrect!');
   alert('Incorrect!');
 } else {
@@ -81,3 +49,72 @@ if(tea.toLowerCase() === 'yes' || tea.toLowerCase() === 'y')
   alert('Please input y/n or yes/no');
 }
 
+var surf = prompt('Do I surf?');
+
+if (surf.toLowerCase() === 'yes' || surf.toLowerCase() === 'y') {
+  console.log('Incorrect!');
+  alert('Incorrect!');
+} else if (surf.toLowerCase() === 'no' || surf.toLowerCase() === 'n') {
+  console.log('Correct!');
+  counter = counter + 1;
+  alert('Correct!');
+} else {
+  console.log('Please input y/n or yes/no');
+  alert('Please input y/n or yes/no');
+}
+
+var tea = prompt('Do I drink tea?');
+
+if (tea.toLowerCase() === 'yes' || tea.toLowerCase() === 'y') {
+  console.log('Correct!');
+  counter = counter + 1;
+  alert('Correct!');
+} else if (tea.toLowerCase() === 'no' || tea.toLowerCase() === 'n') {
+  console.log('Incorrect!');
+  alert('Incorrect!');
+} else {
+  console.log('Please input y/n or yes/no');
+  alert('Please input y/n or yes/no');
+}
+
+var age = parseInt(prompt('Guess my age!'));
+
+for (var i = 1; i < 4; i++) {
+  if (age < 30) {
+    age = parseInt(prompt('Too low! Guess again.'));
+    console.log('Low!');
+  } else if (age > 30) {
+    age = parseInt(prompt('Too high! Guess again.'));
+    console.log('High!');
+  } else if (age === 30) {
+    alert('Correct!');
+    counter = counter + 1;
+    console.log('Correct!');
+    break;
+  } else {
+    age = parseInt(prompt('Type in number!'));
+    console.log('Type number!');
+  }
+}
+
+alert('The correct answer is 30!');
+
+var flavor = prompt('Guess my ice cream flavor.').toLowerCase();
+var myFlavor = ['strawberry', 'chocolate', 'vanilla', 'coffee'];
+
+
+for (var i = 0; i < 5; i++) {
+  if (flavor === myFlavor[0] || flavor === myFlavor[1] || flavor === myFlavor[2] || flavor === myFlavor[3]) {
+    alert('Correct!');
+    counter = counter + 1;
+    console.log('Correct!');
+    break;
+  } else {
+    flavor = prompt('Guess again!').toLowerCase();
+    console.log('Again!');
+  }
+}
+
+alert('The possible correct answers are: strawberry, chocolate, vanilla and coffee.');
+
+alert ('You got ' + counter + ' out of the 7 questions correct');
